8ce74ac2de2fa257fd1426e8e091f50148b2bf22
paulidzko <175623736+paulidzko@users.noreply.github.com>
2026-05-13 14:30:00 +0200

    Added general.geometryAngstrom - retrieve geometry in angstrom

--- molextract/rules/molcas/general.py
+++ molextract/rules/molcas/general.py
@@ -4,6 +4,29 @@ written here
 """
 from molextract.rule import Rule
 
+class geometryAngstrom(Rule):
+    START_TAG = r"                    \*\*\*\* Cartesian Coordinates / bohr, angstrom \*\*\*\*"
+    END_TAG = r"                    \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*"
+
+    def __init__(self):
+        super().__init__(self.START_TAG, self.END_TAG)
+        self.state = []
+
+    def process_lines(self, start_line):
+        self.skip(3)
+        counter = 0 
+        for lines in self:
+            counter += 1
+            if lines != "": # some lines are empty, they cause problems.
+                line = lines.split()
+                xyz = [float(line[5]), float(line[6]), float(line[7])]
+                self.state.append(xyz)
+   
+    def reset(self):
+        tmp = self.state.copy()
+        self.state.clear()
+        return tmp
+
 
 class MolProps(Rule):
     START_TAG = r"\+\+    Molecular properties:"
