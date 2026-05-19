4a93d842c52c1743e18973618b357b07c7ca1f9f
paulidzko <175623736+paulidzko@users.noreply.github.com>
2026-05-19 11:11:38 +0200

    added atom type to general.geometryAngstrom
    changed output

--- molextract/rules/molcas/general.py
+++ molextract/rules/molcas/general.py
@@ -4,6 +4,7 @@ written here
 """
 from molextract.rule import Rule
 
+
 class geometryAngstrom(Rule):
     START_TAG = r"                    \*\*\*\* Cartesian Coordinates / bohr, angstrom \*\*\*\*"
     END_TAG = r"                    \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*"
@@ -15,12 +16,24 @@ class geometryAngstrom(Rule):
     def process_lines(self, start_line):
         self.skip(3)
         counter = 0 
+        props = {
+            "matrix": [], 
+            "atoms": []
+            }
         for lines in self:
-            counter += 1
             if lines != "": # some lines are empty, they cause problems.
                 line = lines.split()
+                counter += 1
+                if counter < 10: # to remove the number from the atoms symbols.
+                    label = str(line[1])[:-1]
+                elif counter < 100:
+                    label = str(line[1])[:-2]
+                else: 
+                    raise Exception("general.atoms can only handle systems up to a size of 99 atoms.")
                 xyz = [float(line[5]), float(line[6]), float(line[7])]
-                self.state.append(xyz)
+                props["matrix"].append(xyz)
+                props["atoms"].append(label)
+        self.state.append(props)
    
     def reset(self):
         tmp = self.state.copy()
