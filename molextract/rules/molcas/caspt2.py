33421626dbc9447668281bc721c51b08d292306c
paulidzko <175623736+paulidzko@users.noreply.github.com>
2026-07-24 11:07:42 +0200

    STARTED... playing with caspt2 xms input

--- molextract/rules/molcas/caspt2.py
+++ molextract/rules/molcas/caspt2.py
@@ -3,14 +3,24 @@ from molextract.rules.abstract import SingleLineRule
 from molextract.rules.molcas import log
 
 
-class CASPT2XmsEnergy(SingleLineRule):
+class CASPT2XmsEnergy(Rule):
 
-    START_TAG = "::    XMS-CASPT2 Root"
+    START_TAG = r"       Total XMS-CASPT2 energies:"#"::    XMS-CASPT2 Root"
+    END_TAG = r"       Eigenvectors:"
 
     def __init__(self):
-        super().__init__(self.START_TAG)
+        super().__init__(self.START_TAG, self.END_TAG)
+        self.state = []
 
     def process(self, line):
-        return float(line.split()[6])
+        for lines in self:
+            line = lines.split()
+        self.state.append(float(line.split()[6]))
+    
+    def reset(self):
+        tmp = self.state.copy()
+        self.state.clear()
+        self.__init__()
+        return tmp
 
 
